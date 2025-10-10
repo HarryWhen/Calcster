f94c14ac9f42b8811d803a415bf3b37f7bd19cc5
Hagai <>
2025-10-10 21:02:36 +0500

    feat: add math operations endpoints to app.calcster.simplecalc

--- app/calcster/simplecalc.py
+++ app/calcster/simplecalc.py
@@ -1,8 +1,40 @@
+from urllib.parse import quote
+
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/simple")
+
+
+class ExprDTO(BaseModel):
+    expr: str
+    url_arg: str
+
+
+def make_expr_dto(expr: str) -> ExprDTO:
+    return ExprDTO(expr=expr, url_arg=quote(expr))
 
 
 @router.get("/")
 def get_calc(expr: str) -> int | float:
     return eval(expr)
+
+
+@router.get("/add")
+def get_sum(l_expr: str, r_expr: str) -> ExprDTO:
+    return make_expr_dto(f"{l_expr}+{r_expr}")
+
+
+@router.get("/sub")
+def get_diff(l_expr: str, r_expr: str) -> ExprDTO:
+    return make_expr_dto(f"{l_expr}-{r_expr}")
+
+
+@router.get("/mul")
+def get_prod(l_expr: str, r_expr: str) -> ExprDTO:
+    return make_expr_dto(f"{l_expr}*{r_expr}")
+
+
+@router.get("/div")
+def get_quot(l_expr: str, r_expr: str) -> ExprDTO:
+    return make_expr_dto(f"{l_expr}/{r_expr}")
