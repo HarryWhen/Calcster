922bec6fd88c561a05fde6f87543a9b0f767d866
Hagai <>
2025-10-12 21:18:17 +0500

    feat: add match operations endpoints to oneoperationcalc

--- app/calcster/oneoperationcalc.py
+++ app/calcster/oneoperationcalc.py
@@ -14,5 +14,34 @@ def get_entry() -> OneOperationInfo:
 
 
 @router.get("/calc")
-def get_calc(expr: str) -> int | float:
-    return eval(expr)
+def get_calc(l_value: int | float, r_value: int | float, op: str) -> int | float:
+    match op:
+        case "+":
+            return get_sum(l_value, r_value)
+        case "-":
+            return get_diff(l_value, r_value)
+        case "*":
+            return get_prod(l_value, r_value)
+        case "/":
+            return get_quot(l_value, r_value)
+    raise ValueError(f"Unknown op: {op}")
+
+
+@router.get("/add")
+def get_sum(l_value: int | float, r_value: int | float) -> int | float:
+    return l_value + r_value
+
+
+@router.get("/sub")
+def get_diff(l_value: int | float, r_value: int | float) -> int | float:
+    return l_value - r_value
+
+
+@router.get("/mul")
+def get_prod(l_value: int | float, r_value: int | float) -> int | float:
+    return l_value * r_value
+
+
+@router.get("/div")
+def get_quot(l_value: int | float, r_value: int | float) -> int | float:
+    return l_value / r_value
