23d455446f9c453152e2c580c8d0068914a72031
Hagai <>
2025-10-19 21:44:05 +0500

    feat: introduce packaging flow

--- app/main.py
+++ app/main.py
@@ -2,3 +2,14 @@ from . import app
 from .calcster import router
 
 app.include_router(router)
+
+
+def run() -> None:
+    import os
+    from pathlib import Path
+
+    from fastapi_cli.cli import dev
+
+    root = Path(__file__).parent.parent
+    os.chdir(root)
+    dev()
